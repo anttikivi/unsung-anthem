77acd0ff4037efb53f13b7e53aaf5d04f142186b
Antti Kivi <anttikiviyo@gmail.com>
2017-06-28 13:17:21 +0300

    Add product file for Unsung Anthem

---
+++ utils/anthem_build_support/anthem_build_support/products/anthem.py
@@ -0,0 +1,13 @@
+# anthem_build_support/products/anthem.py ------------------------*- python -*-
+#
+# This source file is part of the Unsung Anthem open source project and is
+# adapted from the Swift.org open source project.
+#
+# Copyright (c) 2017 Venturesome Stone
+# Licensed under GNU Affero General Public License v3.0
+#
+# ----------------------------------------------------------------------------
+"""
+Unsung Anthem build
+"""
+# ----------------------------------------------------------------------------
