a3f52e9e8a6c228a259f7d126bdba06cceb8498c
Antti Kivi <anttikiviyo@gmail.com>
2017-06-29 16:46:43 +0300

    Add the first C++ files

---
+++ include/anthem/execute.h
@@ -0,0 +1,36 @@
+//===--- execute.h ----------------------------------------------*- C++ -*-===//
+//
+// This source file is part of the Unsung Anthem open source project.
+//
+// Copyright (c) 2017 Venturesome Stone
+// Licensed under GNU Affero General Public License v3.0
+//
+//===----------------------------------------------------------------------===//
+//
+///
+/// \file execute.h
+/// \brief The declaration of the main execution function of the game engine.
+/// \author Antti Kivi
+/// \date 29 June 2017
+/// \copyright Copyright (c) 2017 Venturesome Stone
+/// Licensed under GNU Affero General Public License v3.0
+///
+//===----------------------------------------------------------------------===//
+
+#ifndef ANTHEM_EXECUTE_H
+#define ANTHEM_EXECUTE_H
+
+namespace anthem {
+
+  ///
+  /// \brief The main execution function of the game engine which eventually
+  /// runs the program.
+  ///
+  /// \param argc The number of command line arguments passed to the program.
+  /// \param argv An array containing the command line arguments passed to the
+  /// program.
+  ///
+  void execute(int argc, const char* argv[]);
+}
+
+#endif // !ANTHEM_EXECUTE_H
---
+++ src/execute.cpp
@@ -0,0 +1,27 @@
+//===--- execute.cpp --------------------------------------------*- C++ -*-===//
+//
+// This source file is part of the Unsung Anthem open source project.
+//
+// Copyright (c) 2017 Venturesome Stone
+// Licensed under GNU Affero General Public License v3.0
+//
+//===----------------------------------------------------------------------===//
+//
+///
+/// \file execute.cpp
+/// \brief The definition of the main execution function of the game engine.
+/// \author Antti Kivi
+/// \date 29 June 2017
+/// \copyright Copyright (c) 2017 Venturesome Stone
+/// Licensed under GNU Affero General Public License v3.0
+///
+//===----------------------------------------------------------------------===//
+
+#include "anthem/execute.h"
+
+namespace anthem {
+
+  void execute(int argc, const char* argv[]) {
+
+  }
+}
