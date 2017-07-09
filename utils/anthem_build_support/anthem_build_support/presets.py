6a3e3f67d37aeb1d056f6a2c2a067f4b62035e10
Antti Kivi <anttikiviyo@gmail.com>
2017-07-09 19:21:36 +0300

    Try to change up the parsing of the presets

--- utils/anthem_build_support/anthem_build_support/presets.py
+++ utils/anthem_build_support/anthem_build_support/presets.py
@@ -18,7 +18,7 @@ import diagnostics
 
 def _load_preset_files_impl(preset_file_names, substitutions={}):
     config = ConfigParser.SafeConfigParser(substitutions, allow_no_value=True)
-    if config.read(preset_file_names) == []:
+    if not config.read(preset_file_names):
         diagnostics.fatal("preset file not found (tried "
                           + str(preset_file_names)
                           + ")")
@@ -32,12 +32,10 @@ def _get_preset_options_impl(config, substitutions, preset_name):
     section_name = _PRESET_PREFIX + preset_name
 
     if section_name not in config.sections():
-        return None, None, None
+        return None, None
 
     build_script_opts = []
-    build_script_impl_opts = []
     missing_opts = []
-    dash_dash_seen = False
 
     for o in config.options(section_name):
         try:
@@ -54,49 +52,52 @@ def _get_preset_options_impl(config, substitutions, preset_name):
             continue
 
         opt = None
+        value = None
+
+        print(o)
+
         if o == "mixin-preset":
             # Split on newlines and filter out empty lines.
             mixins = filter(None, [m.strip() for m in a.splitlines()])
             for mixin in mixins:
-                (base_build_script_opts,
-                 base_build_script_impl_opts,
-                 base_missing_opts) = \
+                (base_build_script_opts, base_missing_opts) = \
                     _get_preset_options_impl(config, substitutions, mixin)
                 build_script_opts += base_build_script_opts
-                build_script_impl_opts += base_build_script_impl_opts
                 missing_opts += base_missing_opts
-        elif o == "dash-dash":
-            dash_dash_seen = True
         elif a == "":
             opt = "--" + o
         else:
-            opt = "--" + o + "=" + a
+            # opt = "--" + o + " " + a
+            opt = "--" + o
+            value = a
 
         if opt:
-            if not dash_dash_seen:
-                build_script_opts.append(opt)
-            else:
-                build_script_impl_opts.append(opt)
+            build_script_opts.append(opt)
 
-    return build_script_opts, build_script_impl_opts, missing_opts
+        if value:
+            build_script_opts.append(value)
+
+    return build_script_opts, missing_opts
 
 
 def get_preset_options(substitutions, preset_file_names, preset_name):
     config = _load_preset_files_impl(preset_file_names, substitutions)
 
-    (build_script_opts, build_script_impl_opts, missing_opts) = \
+    (build_script_opts, missing_opts) = \
         _get_preset_options_impl(config, substitutions, preset_name)
-    if not build_script_opts and not build_script_impl_opts:
-        diagnostics.fatal("preset '"
-                          + preset_name
-                          + "' not found")
+
+    if not build_script_opts:
+        diagnostics.fatal("preset '" + preset_name + "' not found")
+
     if missing_opts:
         diagnostics.fatal("missing option(s) for preset '"
                           + preset_name
                           + "': "
                           + ", ".join(missing_opts))
 
-    return build_script_opts + ["--"] + build_script_impl_opts
+    build_script_opts += ["--"]
+
+    return build_script_opts
 
 
 def get_all_preset_names(preset_file_names):
