dbd4a81104a7122e0a870c0985bd4a2c5a69b87d
Antti Kivi <anttikiviyo@gmail.com>
2017-07-31 11:17:31 +0300

    Add the rest of the operators for the not_null type

--- test/gsl/view_test.cpp
+++ test/gsl/view_test.cpp
@@ -52,6 +52,8 @@ TEST_CASE("type cast operator of not_null works", "[not_null]") {
 
   REQUIRE(s == decltype(s)(n));
   REQUIRE(*s == *decltype(s)(n));
+
+  delete s;
 }
 
 TEST_CASE("member access operator of not_null works", "[not_null]") {
@@ -59,6 +61,8 @@ TEST_CASE("member access operator of not_null works", "[not_null]") {
   const gsl::not_null<std::string*> n{s};
 
   REQUIRE(s->size() == n->size());
+
+  delete s;
 }
 
 TEST_CASE("dereference operator of not_null works", "[not_null]") {
@@ -66,6 +70,8 @@ TEST_CASE("dereference operator of not_null works", "[not_null]") {
   const gsl::not_null<std::string*> n{s};
 
   REQUIRE(*s == *n);
+
+  delete s;
 }
 
 TEST_CASE("get() of not_null works", "[not_null]") {
@@ -74,4 +80,41 @@ TEST_CASE("get() of not_null works", "[not_null]") {
 
   REQUIRE(s == n.get());
   REQUIRE(*s == *n.get());
+
+  delete s;
+}
+
+TEST_CASE("== operator of not_null works", "[not_null]") {
+  const auto s = new std::string("Hello, world!");
+  const gsl::not_null<std::string*> n1{s};
+  const gsl::not_null<std::string*> n2{s};
+
+  REQUIRE(n1 == n2);
+
+  delete s;
+}
+
+TEST_CASE("!= operator of not_null works", "[not_null]") {
+  const auto s1 = new std::string("Hello, world!");
+  const auto s2 = new std::string("!dlrow ,olleW");
+  const gsl::not_null<std::string*> n1{s1};
+  const gsl::not_null<std::string*> n2{s2};
+
+  REQUIRE(n1 != n2);
+
+  delete s1;
+  delete s2;
+}
+
+TEST_CASE("hash function of not_null works", "[not_null]") {
+  const auto s = new std::string("Hello, world!");
+  const gsl::not_null<std::string*> n1{s};
+  const gsl::not_null<std::string*> n2{s};
+
+  INFO("The hash of the first not_null is " << std::hash<gsl::not_null<std::string*>>{}(n1));
+  INFO("The hash of the second not_null is " << std::hash<gsl::not_null<std::string*>>{}(n2));
+
+  REQUIRE(std::hash<gsl::not_null<std::string*>>{}(n1) == std::hash<gsl::not_null<std::string*>>{}(n2));
+
+  delete s;
 }
