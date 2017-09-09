121437293d7f47febdea9862d1c92988c901d557
Antti Kivi <anttikiviyo@gmail.com>
2017-09-09 17:17:40 +0300

    Remove the old build script

--- utils-old/anthem_build_support/anthem_build_support/products/__init__.py
+++
@@ -1,6 +0,0 @@
-# products/__init__.py --------------------------------------------*- python -*-
-#
-# This source file is part of the Unsung Anthem open source project.
-#
-# Copyright (c) 2017 Venturesome Stone
-# Licensed under GNU Affero General Public License v3.0
