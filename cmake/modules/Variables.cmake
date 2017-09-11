f45faefc3a33e3a9e52fd9f6c24a34551281ea95
Antti Kivi <anttikiviyo@gmail.com>
2017-09-11 17:38:34 +0300

    Fix the quoted dereferenced variables which are against the newer policy of CMake

--- cmake/modules/Variables.cmake
+++ cmake/modules/Variables.cmake
@@ -12,7 +12,7 @@ function(PRINT_STATUS NAME TITLE)
 endfunction()
 
 function(ASSERT_VARIABLE NAME TITLE POSSIBLE_VALUES)
-  if(NOT DEFINED "${NAME}")
+  if(NOT DEFINED ${NAME})
     message(FATAL_ERROR
         "The ${TITLE} (${NAME}) is not set – the possible values are \
 ${POSSIBLE_VALUES}")
@@ -22,7 +22,7 @@ ${POSSIBLE_VALUES}")
 endfunction()
 
 function(ASSERT_VARIABLE NAME TITLE)
-  if(NOT DEFINED "${NAME}")
+  if(NOT DEFINED ${NAME})
     message(FATAL_ERROR "The ${TITLE} (${NAME}) is not set")
   else()
     print_status(${NAME} ${TITLE})
@@ -30,22 +30,22 @@ function(ASSERT_VARIABLE NAME TITLE)
 endfunction()
 
 function(ASSERT_ONLY_OTHER NAME1 NAME2 TITLE1 TITLE2)
-  if(NOT DEFINED "${NAME1}" AND NOT DEFINED "${NAME2}")
+  if(NOT DEFINED ${NAME1} AND NOT DEFINED ${NAME2})
     message(FATAL_ERROR
         "The neither ${TITLE1} nor ${TITLE2} (${NAME1} and ${NAME2}) is not \
 set")
-  elseif("${NAME1}" AND "${NAME2}")
+  elseif(${NAME1} AND ${NAME2})
     message(FATAL_ERROR
         "The both ${TITLE1} and ${TITLE2} (${NAME1} and ${NAME2}) are used")
-  elseif(NOT "${NAME1}" AND NOT "${NAME2}")
+  elseif(NOT ${NAME1} AND NOT ${NAME2})
     message(FATAL_ERROR
         "The neither ${TITLE1} nor ${TITLE2} (${NAME1} and ${NAME2}) is used")
   endif()
 endfunction()
 
 function(DEFAULT_VALUE NAME VALUE)
-  if(NOT DEFINED "${NAME}")
+  if(NOT DEFINED ${NAME})
     message(STATUS "${NAME} is not set and will be defaulted to ${VALUE}")
-    set("${NAME}" ${VALUE} PARENT_SCOPE)
+    set(${NAME} ${VALUE} PARENT_SCOPE)
   endif()
 endfunction()
