220c4d0cca1b4422f9e3f916d3e9b0c2be753b52
Antti Kivi <anttikiviyo@gmail.com>
2017-09-19 18:12:59 +0300

    Add a couple of functions to fix the Windows build

--- utils/build_support/products/args.py
+++ utils/build_support/products/args.py
@@ -20,6 +20,48 @@ from .product import binary_exists, check_source
 from .. import shell, workspace
 
 
+def args_build_bin_path(build_data):
+    """
+    Create the path for the binary of args in the build directory.
+
+    build_data -- the build data.
+    """
+    build_dir = workspace.build_dir(
+        build_data=build_data, product=build_data.products.args
+    )
+    return os.path.join(build_dir, "args.hxx")
+
+
+def args_bin_path(build_data):
+    """
+    Create the path for the binary of args.
+
+    build_data -- the build data.
+    """
+    return os.path.join(build_data.install_root, "include", "args.hxx")
+
+
+def do_windows_build(build_data):
+    """
+    Do the Windows build of args.
+
+    build_data -- the build data.
+    """
+    product = build_data.products.args
+    bin_path = args_bin_path(build_data=build_data)
+    build_bin_path = args_build_bin_path(build_data=build_data)
+
+    if binary_exists(build_data=build_data, product=product, path=bin_path):
+        return
+
+    if os.path.exists(build_bin_path):
+        shell.rm(bin_path)
+        if not os.path.isdir(os.path.join(build_data.install_root, "include")):
+            shell.makedirs(os.path.join(build_data.install_root, "include"))
+        shell.copy(build_bin_path, bin_path)
+        return
+
+
 def build(build_data):
     """
     Build args.
@@ -36,9 +78,7 @@ def build(build_data):
     shell.rmtree(build_dir)
     shell.copytree(source_dir, build_dir)
     if platform.system() == "Windows":
-        shell.copy(
-            os.path.join(build_dir, "args.hxx"),
-            os.path.join(build_data.install_root, "install", "args.hxx"))
+        do_windows_build(build_data=build_data)
     else:
         with shell.pushd(build_dir):
             shell.make(
