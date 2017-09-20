71a481996585cd3eef9295095e6e0d3e4a242c9c
Antti Kivi <anttikiviyo@gmail.com>
2017-09-21 01:53:40 +0300

    Try to use the ld

--- cmake/modules/CompilerFlags.cmake
+++ cmake/modules/CompilerFlags.cmake
@@ -11,7 +11,7 @@ function(SET_STD_FLAGS STD)
   if(${ANTHEM_CI_LLVM})
     set(CMAKE_CXX_FLAGS
         "${CMAKE_CXX_FLAGS} -nostdinc++ \
--Wl,-rpath,${ANTHEM_INSTALL_PREFIX}/lib")
+-Wl,-rpath,${ANTHEM_INSTALL_PREFIX}/lib -lc++")
   endif()
 
   if (${ANTHEM_MAIN_COMPILER_TOOL} STREQUAL msbuild)
