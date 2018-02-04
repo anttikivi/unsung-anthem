292727de7c58cecfbe94b44cc2b2f676280768fd
Antti Kivi <anttikiviyo@gmail.com>
2018-02-04 22:25:06 +0200

    Add different build for hayai on Windows

--- utils/products/hayai/build.py
+++ utils/products/hayai/build.py
@@ -14,6 +14,7 @@ The support module containing the utilities for hayai build.
 
 
 import os
+import platform
 
 from build_utils import shell, workspace
 
@@ -22,26 +23,51 @@ from products import common
 from script_support import data
 
 
+def _build_windows():
+    product = data.build.products.hayai
+    source_dir = workspace.source_dir(product)
+    include_dir = workspace.include_file(path=product.key)
+    if not workspace.is_include_dir_made() \
+            and workspace.include_file_exists(path=product.key):
+        shell.rmtree(include_dir)
+    shell.makedirs(include_dir)
+    for header in os.listdir(os.path.join(source_dir, "src")):
+        if not header.endswith(".cpp") and not header.endswith(".txt"):
+            shell.copy(
+                os.path.join(source_dir, "src", header),
+                os.path.join(include_dir, header)
+            )
+
+
+def _build():
+    build_dir = workspace.build_dir(product)
+    shell.makedirs(build_dir)
+    cmake_args = {
+        "INSTALL_LIB_DIR": os.path.join(data.build.local_root, "lib"),
+        "INSTALL_BIN_DIR": os.path.join(data.build.local_root, "bin"),
+        "INSTALL_INCLUDE_DIR": os.path.join(data.build.local_root, "include")
+    }
+    common.build.build_call(product=product, cmake_args=cmake_args)
+
+
 def do_build():
     """
     Build hayai.
     """
     product = data.build.products.hayai
     common.build.check_source(product)
-    bin_path = os.path.join(data.build.local_root, "lib", "libhayai_main.a")
+    bin_path = os.path.join(
+        data.build.local_root,
+        "include",
+        product.key,
+        "hayai.hpp"
+    )
     if common.build.binary_exists(product=product, path=bin_path):
         return
-    build_dir = workspace.build_dir(product)
-
-    shell.makedirs(build_dir)
-
-    cmake_args = {
-        "INSTALL_LIB_DIR": os.path.join(data.build.local_root, "lib"),
-        "INSTALL_BIN_DIR": os.path.join(data.build.local_root, "bin"),
-        "INSTALL_INCLUDE_DIR": os.path.join(data.build.local_root, "include")
-    }
-
-    common.build.build_call(product=product, cmake_args=cmake_args)
+    if platform.system() == "Windows":
+        _build_windows()
+    else:
+        _build()
 
 
 def should_build():
