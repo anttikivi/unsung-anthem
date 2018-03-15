d2652ec754fc4271a74a5d1d3ca56325a101f224
Antti Kivi <anttikiviyo@gmail.com>
2018-03-15 22:17:24 +0200

    Try copying the gtest files

--- utils/products/googletest/build.py
+++ utils/products/googletest/build.py
@@ -23,17 +23,27 @@ from products import common
 from script_support import data
 
 
-def _build():
+def _copy_windows():
     product = data.build.products.googletest
-    common.build.check_source(product)
-    bin_path = os.path.join(data.build.local_root, "lib", "libgoogletest.a")
     build_dir = workspace.build_dir(product)
-    if common.build.binary_exists(product=product, path=bin_path):
-        return
-    shell.makedirs(build_dir)
-    common.build.build_call(product=product, cmake_args={
-        "CMAKE_CXX_FLAGS": "/D_SILENCE_TR1_NAMESPACE_DEPRECATION_WARNING"
-    }, solution_name="gtest", source_subdir="googletest")
+    if not workspace.is_lib_dir_made():
+        if os.path.exists(workspace.lib_file(path="gtest.lib")):
+            shell.rm(workspace.lib_file(path="gtest.lib"))
+    shell.copy(
+        os.path.join(build_dir, "Debug", "gtest.lib"),
+        workspace.lib_file(path="gtest.lib"))
+    if not workspace.is_lib_dir_made():
+        if workspace.include_file_exists(path="gtest"):
+            shell.rmtree(workspace.include_file(path="gtest"))
+    source_dir = workspace.source_dir(product=product)
+    shell.copytree(
+        os.path.join(source_dir, "googletest", "include", "gtest"),
+        os.path.join(data.build.local_root, "include", "gtest"))
+    for dirpath, dirnames, filenames in os.walk(data.build.local_root):
+        print("Now going through directory '{}'".format(dirpath))
+        for name in filenames:
+            path = os.path.join(dirpath, name)
+            print(path)
 
 
 def do_build():
@@ -41,10 +51,10 @@ def do_build():
     product = data.build.products.googletest
     common.build.check_source(product)
     if platform.system() == "Windows":
-        bin_path = os.path.join(data.build.local_root, "lib", "googletest.lib")
+        bin_path = os.path.join(data.build.local_root, "lib", "gtest.lib")
     else:
         bin_path = os.path.join(
-            data.build.local_root, "lib", "libgoogletest.a")
+            data.build.local_root, "lib", "libgtest.a")
     build_dir = workspace.build_dir(product)
     if common.build.binary_exists(product=product, path=bin_path):
         return
