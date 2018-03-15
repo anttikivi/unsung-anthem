4aafcc37880bcf0b936839377a325db99ad00603
Antti Kivi <anttikiviyo@gmail.com>
2018-03-15 23:40:15 +0200

    :checkered_flag: Use the flag on Windows with tests to avoid errors

--- utils/products/common/final/cmake.py
+++ utils/products/common/final/cmake.py
@@ -173,6 +173,10 @@ def construct_call(is_ode=False, lib=False, test=False):
     else:
         cmake_call += ["-DODE_LINK_LIBCXX=OFF"]
 
+    if test and platform.system() == "Windows":
+        cmake_call += \
+            ["CMAKE_CXX_FLAGS=/D_SILENCE_TR1_NAMESPACE_DEPRECATION_WARNING"]
+
     if args.extra_cmake_options:
         cmake_call += args.extra_cmake_options
 
