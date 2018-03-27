cfd5cbace5a148eb285b7579897a7c97e41aada4
Antti Kivi <anttikiviyo@gmail.com>
2018-03-27 13:54:06 +0300

    Use the new rpath variable

--- cmake/modules/CompilerFlags.cmake
+++ cmake/modules/CompilerFlags.cmake
@@ -27,15 +27,11 @@ function(SET_STD_FLAGS STD)
 endfunction()
 
 function(SET_COMPILER_FLAGS)
-  if(ODE_SET_RPATH)
-    # TODO Should the rpath be set to the ‘lib’ subdirectory of install
-    # directory.
-    set(CMAKE_CXX_FLAGS
-        "${CMAKE_CXX_FLAGS} -Wl,-rpath,${CMAKE_INSTALL_PREFIX}/bin")
+  if(DEFINED ODE_RPATH)
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wl,-rpath,${ODE_RPATH}")
   endif()
   if(DEFINED ODE_OPTIMIZATION_LEVEL)
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -O${ODE_OPTIMIZATION_LEVEL}")
   endif()
   set(CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS} PARENT_SCOPE)
 endfunction()
-
