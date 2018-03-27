2111cbde00a552d0a05bed25f8e4b177356ff9b1
Antti Kivi <anttikiviyo@gmail.com>
2018-03-27 13:02:51 +0300

    Add the correct sources for Google Benchmark

---
+++ cmake/modules/Logging.cmake
@@ -0,0 +1,13 @@
+#===---------------------------- Logging.cmake -----------------------------===#
+#
+#                         Obliging Ode & Unsung Anthem
+#
+# This source file is part of the Obliging Ode and Unsung Anthem open source
+# projects.
+#
+# Copyright (c) 2018 Venturesome Stone
+# Licensed under GNU Affero General Public License v3.0
+
+function(INFO MESSAGE)
+  message(STATUS "${MESSAGE}")
+endfunction()
