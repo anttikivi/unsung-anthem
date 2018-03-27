3163fc288d9b1c98480537432331db69158178c9
Antti Kivi <anttikiviyo@gmail.com>
2018-03-27 22:37:15 +0300

    Add configuration file for the build script

---
+++ utils/config.py
@@ -0,0 +1,58 @@
+#===------------------------------ config.py ------------------*- python -*-===#
+#
+#                         Obliging Ode & Unsung Anthem
+#
+# This source file is part of the Obliging Ode and Unsung Anthem open source
+# projects.
+#
+# Copyright (c) 2018 Venturesome Stone
+# Licensed under GNU Affero General Public License v3.0
+
+"""Default option value definitions."""
+
+
+ANTHEM_VERSION = "0.1.0-dev.1"
+ANTHEM_NAME = "Unsung Anthem"
+ANTHEM_KEY = "anthem"
+ANTHEM_WINDOW_NAME = "Unsung Anthem"
+ANTHEM_LOGGER_NAME = "anthem"
+
+
+BENCHMARK_VERSION = "1.3.0"
+
+
+CLARA_VERSION = "1.1.4"
+
+
+CMAKE_MAJOR_VERSION = 3
+CMAKE_MINOR_VERSION = 8
+CMAKE_PATCH_VERSION = 2
+
+
+GLAD_VERSION = "0.1.18a0"
+
+
+GOOGLETEST_VERSION = "1.8.0"
+
+
+LLVM_VERSION = "6.0.0"
+
+
+LUA_VERSION = "5.3.4"
+
+
+NINJA_VERSION = "1.8.2"
+
+
+ODE_VERSION = "0.1.0-dev.1",
+ODE_NAME = "Obliging Ode",
+ODE_KEY = "ode",
+ODE_OPENGL_MAJOR_VERSION = 3
+ODE_OPENGL_MINOR_VERSION = 2
+ODE_LOGGER_NAME = "ode",
+
+
+SDL_VERSION = "2.0.8"
+
+
+SPDLOG_VERSION = "0.16.3"
