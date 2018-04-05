3d4571c052543a95f3fce73355df8f84d15a9972
Antti Kivi <anttikiviyo@gmail.com>
2018-04-05 21:01:42 +0300

    :art: Fix the copyright year

--- include/ode/lua/state.h
+++ include/ode/lua/state.h
@@ -15,7 +15,7 @@
 /// \brief The declaration of the Lua state.
 /// \author Antti Kivi
 /// \date 6 February 2018
-/// \copyright Copyright (c) 2017 Venturesome Stone
+/// \copyright Copyright (c) 2018 Venturesome Stone
 /// Licensed under GNU Affero General Public License v3.0
 ///
 //
