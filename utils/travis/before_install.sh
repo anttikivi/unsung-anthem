4ee83401b01a84bcc6e50e701db23cca73cc44dc
Antti Kivi <anttikiviyo@gmail.com>
2018-04-06 14:09:40 +0300

    :green_heart: Call which from a separate script

---
+++ utils/travis/before_install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#===-------------------------- before_install.sh ---------------------------===#
+#
+#                         Obliging Ode & Unsung Anthem
+#
+# This source file is part of the Obliging Ode and Unsung Anthem open source
+# projects.
+#
+# Copyright (c) 2018 Venturesome Stone
+# Licensed under GNU Affero General Public License v3.0
+
+set -v
+
+${CC} --version
+${CXX} --version
+which python
+which python2
+which python3
+which pip
+which pip2
+which pip3
