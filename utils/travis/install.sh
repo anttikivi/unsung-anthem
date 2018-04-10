7812555e7fa112b27a5e72071d4116c169b58ce5
Antti Kivi <anttikiviyo@gmail.com>
2018-04-10 17:12:24 +0300

    :green_heart: Move the sudo commands to the shell scripts

--- utils/travis/install.sh
+++ utils/travis/install.sh
@@ -14,8 +14,8 @@ set -ev
 
 if [ "${ENABLE_COVERAGE}" == "true" ]; then
   cd /
-  mkdir unsung-anthem
-  cp -a ${TRAVIS_BUILD_DIR}/unsung-anthem/. /unsung-anthem/
+  sudo mkdir unsung-anthem
+  sudo cp -a ${TRAVIS_BUILD_DIR}/unsung-anthem/. /unsung-anthem/
 fi
 
 ${ODE_PYTHON:-python} unsung-anthem/utils/build-script --develop-script -v
