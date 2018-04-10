b308936f87a6071e291ce72a48e7f4f2f5c6fda5
Antti Kivi <anttikiviyo@gmail.com>
2018-04-10 11:39:52 +0300

    :hammer: Add option for using the SDL clock instead of standard library clock

--- cmake/modules/Definitions.cmake
+++ cmake/modules/Definitions.cmake
@@ -53,6 +53,12 @@ function(ADD_FEATURE_DEFINITIONS)
     add_definitions(-DANTHEM_TEST_BENCHMARKING=1)
   endif()
 
+  if(ODE_SDL_TICK_CLOCK)
+    add_definitions(-DODE_SDL_TICK_CLOCK=1)
+  else()
+    add_definitions(-DODE_SDL_TICK_CLOCK=0)
+  endif()
+
   if(ODE_TEST_USE_NULL_SINK)
     add_definitions(-DODE_TEST_USE_NULL_SINK=1)
   else()
