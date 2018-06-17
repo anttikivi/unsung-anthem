118fc772fc1a54dc0bf33b4ece0e84d90db32c37
Antti Kivi <anttikiviyo@gmail.com>
2018-06-17 19:18:01 +0300

    :fire: Use the same map for the tests and the game

--- test/anthem/systems/scenes/world/map_loading_test.cpp
+++ test/anthem/systems/scenes/world/map_loading_test.cpp
@@ -35,33 +35,31 @@
 TEST(anthem_systems_scenes_world_map_loading, width)
 {
   auto state = ode::lua::make_state();
-  std::string s = std::string{anthem::test_script_root}
-      + ode::filesystem::path::preferred_separator
-      + "systems"
-      + ode::filesystem::path::preferred_separator
-      + "scenes"
+  constexpr auto name = "test";
+  std::string s = std::string{anthem::script_root}
       + ode::filesystem::path::preferred_separator
       + "world"
       + ode::filesystem::path::preferred_separator
+      + name
+      + ode::filesystem::path::preferred_separator
       + "map.lua";
   ode::lua::load_script_file(state.get(), s);
-  const auto width = anthem::world::load_map_width(state.get(), "test");
+  const auto width = anthem::world::load_map_width(state.get(), name);
   ASSERT_EQ(width, 100);
 }
 
 TEST(anthem_systems_scenes_world_map_loading, height)
 {
   auto state = ode::lua::make_state();
-  std::string s = std::string{anthem::test_script_root}
-      + ode::filesystem::path::preferred_separator
-      + "systems"
-      + ode::filesystem::path::preferred_separator
-      + "scenes"
+  constexpr auto name = "test";
+  std::string s = std::string{anthem::script_root}
       + ode::filesystem::path::preferred_separator
       + "world"
       + ode::filesystem::path::preferred_separator
+      + name
+      + ode::filesystem::path::preferred_separator
       + "map.lua";
   ode::lua::load_script_file(state.get(), s);
-  const auto height = anthem::world::load_map_height(state.get(), "test");
+  const auto height = anthem::world::load_map_height(state.get(), name);
   ASSERT_EQ(height, 100);
 }
