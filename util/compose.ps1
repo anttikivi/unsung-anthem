3b210c79f9e4bbd1b89a841f63dd0a542f3a972f
Antti Kivi <antti.kivi@visiosto.fi>
2019-12-28 06:05:57 +0200

    Add dummy PowerShell scripts as placeholders for CI

---
+++ util/compose.ps1
@@ -0,0 +1,17 @@
+#!/usr/bin/env pwsh
+
+# ---------------------------------------------------------------------------- #
+#                         Obliging Ode & Unsung Anthem
+# ---------------------------------------------------------------------------- #
+#
+# This source file is part of the Obliging Ode and Unsung Anthem projects.
+#
+# Copyright (c) 2019 Antti Kivi
+# All rights reserved
+#
+# ---------------------------------------------------------------------------- #
+
+New-Variable ComposerVersion "0.4.5" -Option Constant
+New-Variable ComposerName "Couplet Composer" -Option Constant
+
+Write-Output "Running the PowerShell implementation of the runner of $ComposerName in configuring mode"
