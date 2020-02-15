17bd3bde4cdfad8a8db8d7d930bb666e98007f2f
Antti Kivi <antti.kivi@visiosto.fi>
2020-02-16 00:38:10 +0200

    Update Couplet Composer to 0.5.3

--- util/configure.ps1
+++ util/configure.ps1
@@ -11,7 +11,7 @@
 #
 # ---------------------------------------------------------------------------- #
 
-New-Variable ComposerVersion "0.5.2" -Option Constant
+New-Variable ComposerVersion "0.5.3" -Option Constant
 New-Variable ComposerVersionTag "v$ComposerVersion" -Option Constant
 New-Variable ComposerName "Couplet Composer" -Option Constant
 New-Variable OdeName "Obliging Ode" -Option Constant
