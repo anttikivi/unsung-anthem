e208d6dc6926101dca552de21c8d3a3ef448f9d1
Antti Kivi <antti.kivi@visiosto.fi>
2020-03-15 16:04:40 +0200

    Upgrade Couplet Composer to 0.7.6

--- util/set_up.ps1
+++ util/set_up.ps1
@@ -11,7 +11,7 @@
 #
 # ---------------------------------------------------------------------------- #
 
-New-Variable ComposerVersion "0.7.4" -Option Constant
+New-Variable ComposerVersion "0.7.6" -Option Constant
 New-Variable ComposerVersionTag "v$ComposerVersion" -Option Constant
 New-Variable ComposerName "Couplet Composer" -Option Constant
 New-Variable OdeName "Obliging Ode" -Option Constant
