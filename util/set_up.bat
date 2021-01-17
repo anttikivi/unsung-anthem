535110e511f74e2589f4b69252d1d750287cd891
Antti Kivi <antti.kivi@visiosto.fi>
2021-01-17 13:49:43 +0200

    Add environment variable for using the local development version of Composer

--- util/set_up.bat
+++ util/set_up.bat
@@ -40,6 +40,7 @@ set composer_version_temporary=%composer_version:.=_%
 set composer_directory_name=%composer_directory_base_name%_^
 %composer_version_temporary:-=_%
 set composer_head_directory_name=%composer_directory_base_name%_head
+set composer_local_directory_name=%composer_directory_base_name%_local
 
 setlocal enabledelayedexpansion
 
@@ -75,12 +76,12 @@ echo Created the directory '%script_directory%'
 if defined ODE_DEVELOPMENT_COMPOSER (
   goto development_variable_is_defined
 else (
-  goto get_composer_release
+  goto check_local_variable
 )
 
 :development_variable_is_defined
 if [%ODE_DEVELOPMENT_COMPOSER%]==[] (
-  goto get_composer_release
+  goto check_local_variable
 ) else (
   goto development_variable_is_set
 )
@@ -88,6 +89,27 @@ if [%ODE_DEVELOPMENT_COMPOSER%]==[] (
 :development_variable_is_set
 if %ODE_DEVELOPMENT_COMPOSER%==true (
   goto get_development_composer
+) else (
+  goto check_local_variable
+)
+
+:check_local_variable
+if defined ODE_LOCAL_COMPOSER (
+  goto local_variable_is_defined
+else (
+  goto get_composer_release
+)
+
+:local_variable_is_defined
+if [%ODE_LOCAL_COMPOSER%]==[] (
+  goto get_composer_release
+) else (
+  goto local_variable_is_set
+)
+
+:local_variable_is_set
+if %ODE_LOCAL_COMPOSER%==true (
+  goto get_local_composer
 ) else (
   goto get_composer_release
 )
@@ -107,6 +129,8 @@ goto install_composer
 echo Using development version of %composer_name%
 
 set composer_directory=%script_directory%\%composer_head_directory_name%
+set composer_remote=origin
+set composer_remote_branch=%composer_remote_branch%/develop
 
 if not exist %composer_directory% (
   goto get_dev_no_dir
@@ -133,16 +157,16 @@ start "git fetch" /w /b git -C %composer_directory% fetch
 
 for /f %%i in ('git -C %composer_directory% rev-parse HEAD') do set head_sha=%%i
 
-for /f %%i in ('git -C %composer_directory% rev-parse origin/develop') do (
-  set origin_sha=%%i
+for /f %%i in ('git -C %composer_directory% rev-parse %composer_remote_branch%') do (
+  set remote_sha=%%i
 )
 
 echo The SHA1 for the currently cloned HEAD of %composer_name% is %head_sha%
-echo The SHA1 for origin/develop of %composer_name% is %origin_sha%
+echo The SHA1 for %composer_remote_branch% of %composer_name% is %remote_sha%
 
-if not %head_sha%==%origin_sha% (
-  echo The SHA1's for origin/develop and local HEAD of %composer_name% don't ^
-match and thus the local copy will be reset
+if not %head_sha%==%remote_sha% (
+  echo The SHA1's for %composer_remote_branch% and local HEAD of ^
+%composer_name% don't match and thus the local copy will be reset
   start "git reset" /w /b git -C %composer_directory% reset --hard HEAD
   start "git clean" /w /b git -C %composer_directory% clean -xffd
   start "git pull" /w /b git -C %composer_directory% pull
@@ -152,6 +176,78 @@ goto finalize_dev_setup
 
 :finalize_dev_setup
 echo The latest development version of %composer_name% is now set up
+goto install_composer
+
+:get_local_composer
+echo Using local development version of %composer_name%
+
+set composer_directory=%script_directory%\%composer_local_directory_name%
+set composer_local_repo=%cd%/couplet-composer
+set composer_remote=origin
+
+if not exist %composer_directory% (
+  goto get_local_no_dir
+) else (
+  goto get_local_existing_dir
+)
+
+:get_local_no_dir
+@rem If the Composer directory doesn't exist, there is no need to check for
+@rem the local repository HEAD SHA1.
+echo The directory for '%composer_directory%' doesn't exist and, thus, ^
+%composer_name% will be cloned from the local repository
+
+start "git clone" /w /b git clone %composer_local_repo% %composer_directory%
+
+for /f %%i in ('git -C %composer_directory% rev-parse HEAD') do set head_sha=%%i
+
+echo The SHA1 for the currently cloned local HEAD of %composer_name% is ^
+%head_sha%
+
+goto finalize_local_setup
+
+:get_local_existing_dir
+for /f %%i in ('git -C %composer_directory% rev-parse --abbrev-ref HEAD') do (
+  set current_branch=%%i
+)
+
+for /f %%i in ('git -C %composer_local_repo% rev-parse --abbrev-ref HEAD') do (
+  set local_branch=%%i
+)
+
+echo The current branch in %composer_directory% is %current_branch%
+echo The current branch of the remote %composer_remote% is %local_branch%
+
+if not %current_branch%==%local_branch% (
+  start "git fetch" /w /b git -C %composer_directory% fetch %composer_remote%
+  start "git checkout" /w /b git -C %composer_directory% checkout -b ^
+%local_branch% %composer_remote%/%local_branch%
+)
+
+start "git fetch" /w /b git -C %composer_directory% fetch %composer_remote%
+
+for /f %%i in ('git -C %composer_directory% rev-parse HEAD') do set head_sha=%%i
+
+for /f %%i in ('git -C %composer_directory% rev-parse %composer_remote%/%local_branch%') do (
+  set remote_sha=%%i
+)
+
+echo The SHA1 for the currently cloned HEAD of %composer_name% is %head_sha%
+echo The SHA1 for %composer_remote%/%local_branch% of %composer_name% is ^
+%remote_sha%
+
+if not %head_sha%==%remote_sha% (
+  echo The SHA1's for %composer_remote%/%local_branch% and local HEAD of ^
+%composer_name% don't match and thus the local copy will be reset
+  start "git reset" /w /b git -C %composer_directory% reset --hard HEAD
+  start "git clean" /w /b git -C %composer_directory% clean -xffd
+  start "git pull" /w /b git -C %composer_directory% pull
+)
+
+goto finalize_local_setup
+
+:finalize_local_setup
+echo The latest local development version of %composer_name% is now set up
 goto install_composer
 
 :install_composer
